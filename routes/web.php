a28a9c5db1a5d6a46135c453a6e9cf6c2f497b0d
Anton <kokonacci@gmail.com>
2022-11-24 10:37:56 +0700

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CustomerController;
 
 /*
@@ -18,7 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/customer', CustomerController::class);
+});
 
-Route::resource('/customer', CustomerController::class);
-
+require __DIR__.'/auth.php';
